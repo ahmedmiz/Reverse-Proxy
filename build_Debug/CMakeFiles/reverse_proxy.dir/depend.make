b84b3cd40c27b45117e94400a41c8d6a236e51eb
ahmed saad <mizoahmed017@gmail.com>
2025-04-21 15:24:35 +0200

    Update .gitignore and untrack ignored files

--- build_Debug/CMakeFiles/reverse_proxy.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for reverse_proxy.
-# This may be replaced when dependencies are built.
--- build_Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mizoahmed017/downloads/RP
-# Build directory: /Users/mizoahmed017/downloads/RP/build_Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[ConfigTests]=] "/Users/mizoahmed017/downloads/RP/build_Debug/bin/test_config")
-set_tests_properties([=[ConfigTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/mizoahmed017/downloads/RP/CMakeLists.txt;111;add_test;/Users/mizoahmed017/downloads/RP/CMakeLists.txt;0;")
